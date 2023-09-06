7707e31cf1bf7bb627abb24de81052320fd4cfea
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-07 08:22:35 +0900

    ヘッダーの作成

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React from 'react'
+import { AiFillHome } from 'react-icons/ai'
+import { BsFillInfoCircleFill } from 'react-icons/bs'
+
+import MenuItem from '@/components/MenuItem'
+
+const Header = () => {
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
