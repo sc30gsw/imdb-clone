93e4473c3fa78906b949abe15b4a3a89ebfb8c79
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-08 09:28:17 +0900

    loadingページの作成

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center mt-10">
+      <div className="loader" />
+    </div>
+  )
+}
+
+export default Loading
